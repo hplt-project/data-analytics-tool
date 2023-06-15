f3df77dca320e8d5d2237335dfb133f4d4b669c9
Marta Bañón <mbanon@prompsit.com>
2023-06-15 12:00:38 +0200

    Added upload interface + processing triggered from webserver

--- CATPOC/server.py
+++ CATPOC/server.py
@@ -1,7 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import subprocess
 import sys
 import os
 import json
+import cgi
+import shutil 
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
@@ -11,6 +14,90 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         self.send_header("Expires", "0")
         SimpleHTTPRequestHandler.end_headers(self)
 
+    def save_file(self, file, filename):
+        outpath = os.path.join(".", "test_corpora", filename)
+
+        with open(outpath, 'wb') as fout:
+            #shutil.copyfileobj(file, fout, 100000)
+            fout.write(file)
+        return outpath
+        
+            
+    def do_upload(self):
+        print ("in post method")
+       
+        #self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        self.send_response(200)
+        self.end_headers()
+                
+        '''
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
+        print(ctype)
+        print(pdict)
+        if ctype == 'multipart/form-data':
+            pdict['boundary'] = bytes(pdict['boundary'], 'utf-8')
+            fields = cgi.parse_multipart(self.rfile, pdict)
+            messagecontent = fields.get('message')[0].decode('utf-8')
+        else:
+            print("WHAT")
+        output = ''
+        output += '<html><body>'
+        output += '<h2> Okay, how about this: </h2>'
+        output += '<h1> %s </h1>' % messagecontent
+        output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+        output += '</html></body>'
+        self.wfile.write(output.encode('utf-8'))
+        print(output)
+        '''
+        '''
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={
+                'REQUEST_METHOD': 'POST',
+                'CONTENT_TYPE': self.headers['Content-Type'],
+            })
+        '''
+        form=cgi.FieldStorage(fp=self.rfile, headers=self.headers,environ={'REQUEST_METHOD':'POST'})
+        
+        corpus = form.getvalue('corpus')
+        corpusname = form.getvalue('corpusname')        
+        srclang = form.getvalue('srclang')
+        trglang = form.getvalue('trglang')
+        
+        saved_file_path = self.save_file(corpus, corpusname)
+        yaml_file_path = saved_file_path.replace("/test_corpora/", "/yaml_dir/") + ".yaml"
+        
+        options = []
+        options.append("python3")
+        options.append("./scripts/readcorpus.py")
+        options.append(saved_file_path)
+        options.append(yaml_file_path)
+        options.append(srclang)
+        options.append(trglang)
+        print(" ".join(options))
+        subprocess.Popen(options)
+
+
+        
+        replyhtml = """
+        <html>
+        <body>
+        <div>
+        Processing! This might take some time.
+        </div>
+        <div>
+        <a href="viewer.html">Go to viewer</a>
+        </div>
+        </body>
+        </html>
+        """
+        self.wfile.write(bytes(replyhtml, 'utf-8'))
+        
+
     def do_list(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
@@ -19,16 +106,25 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(os.listdir("yaml_dir"), ensure_ascii=False), 'utf-8'))
         #str(os.listdir("yaml_dir")))  
         return
+    
+    def do_POST(self):
+        if self.path == '/upload':
+            self.do_upload()
 
     def do_GET(self):
+        #if self.path == '/uploader.html':
+            
         if self.path == '/list':
             self.do_list()
-        else:
+        elif  "/file/" in self.path:
             self.send_response(200)
             self.send_header('Content-type', 'application/text')
             self.end_headers()
-            with open("."+self.path, 'rb') as file: 
+            with open("."+self.path.replace("/file/", "/yaml_dir/"), 'rb') as file: 
                 self.wfile.write(file.read()) # Read the file and send the contents 
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
+        
 
 
                     
