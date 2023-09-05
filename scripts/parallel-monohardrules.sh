f167acab250319debcb83539b13ec3777893a749
mbanon <mbanon@prompsit.com>
2023-09-05 09:14:12 +0000

    Added missing parameters to parallel monohardrules

--- scripts/parallel-monohardrules.sh
+++ scripts/parallel-monohardrules.sh
@@ -4,4 +4,4 @@ lang=$2
 inputfile=$3
 outputfile=$4
 
-cat $inputfile | /work/preprocess/build/bin/cache -k 1   parallel -k -j $JOBS --pipe ./scripts/par-monohardrules.sh $lang > $outputfile
+cat $inputfile | /work/preprocess/build/bin/cache -k 1   parallel -k -j $JOBS --pipe ./scripts/par-monohardrules.sh $lang - - > $outputfile
