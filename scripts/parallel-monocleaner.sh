4d73569fd0c4c1f8919464b66891aa52c118ff62
Marta Bañón <mbanon@prompsit.com>
2023-09-07 16:18:17 +0200

    Remove unused files

--- scripts/parallel-monocleaner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JOBS=$1
-langpath=$2
-inputfile=$3
-outputfile=$4
-
-cat $inputfile | /work/preprocess/build/bin/cache -k 1  parallel -k  -j $JOBS --pipe ./scripts/par-monocleaner.sh $langpath - - > $outputfile
