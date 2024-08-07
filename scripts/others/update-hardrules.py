2728940ed8ac4cce29d90fe780bc90bcf449b1fa
mbanon <mbanon@prompsit.com>
2024-08-07 11:29:57 +0000

    Update yamls to contain absolute figures for hardrules

--- scripts/others/update-hardrules.py
+++ scripts/others/update-hardrules.py
@@ -6,21 +6,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument('yaml_file', help='YAML file to obtain the new hardrules count from.')
 args = parser.parse_args()
 
+data = None
+
 with open(args.yaml_file, "r") as file:
     data =  yaml.load(file, Loader=yaml.FullLoader)
+    
     total_sents = data.get("sentence_pairs")
     hardrules = json.loads(data.get("hardrules_tags"))
-    
+    new_hardrules =  {}
+        
     for tag in hardrules:
         tag_percent = hardrules[tag]
         tag_count = int(total_sents * tag_percent / 100)
-        print("Total: " + str(total_sents))
-        print(tag + ": " + str(tag_percent) + " --> " + str(tag_count))
+        new_hardrules[tag] = tag_count
+    data["hardrules_tags"] = json.dumps(new_hardrules)
         
-         
 
-#data["src_tokens"] = total
-
-#with open(args.yaml_file, "w") as file:
-#    yaml.dump(data,file)    
+with open(args.yaml_file, "w") as file:
+    yaml.dump(data,file)    
 
