59b3ef2e827136f968b10542887b971df639e900
mbanon <mbanon@prompsit.com>
2024-08-20 15:46:42 +0000

    Added Plateau Malagasy

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -24,7 +24,7 @@ logging.disable(logging.NOTSET)
 
 MOSES_LANGS = ["ba", "br", "ca", "cs", "co", "de", "el", "en", "es", "fi", "fr", "hu", "is", "it", "lv", "nl", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "ta"]
 
-NLTK_WORD_LANGS = ["ar", "az", "be", "fa", "hy", "jv","ka", "kbp", "kk", "ky", "mn", "ms", "ps", "tt", "uk", "ug",  "vi", "xh"]
+NLTK_WORD_LANGS = ["ar", "az", "be", "fa", "hy", "jv","ka", "kbp", "kk", "ky", "mn", "ms", "plt", "ps", "tt", "uk", "ug",  "vi", "xh"]
 NLTK_PUNKT_LANGS = {"no": "norwegian",
                     "et": "estonian",
                     "da": "danish",
