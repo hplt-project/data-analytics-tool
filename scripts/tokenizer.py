17db25c94c5ce0fee94a077f0b066fb3a53c3424
mbanon <mbanon@prompsit.com>
2024-08-22 14:56:32 +0000

    Support for Nigerian Fulfulde

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -30,7 +30,7 @@ logging.disable(logging.NOTSET)
 
 MOSES_LANGS = ["ba", "br", "ca", "cs", "co", "de", "el", "en", "es", "fi", "fr", "hu", "is", "it", "lv", "nl", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "ta"]
 
-NLTK_WORD_LANGS = ["ar", "ayr", "az", "be", "ceb", "fa", "ht", "hy", "jv", "ka", "kbp", "kea", "kk", "ky", "mn", "ms", "plt", "ps", "tt", "tw", "uk", "ug",  "vi", "xh"]
+NLTK_WORD_LANGS = ["ar", "ayr", "az", "be", "ceb", "fa", "fuv", "ht", "hy", "jv", "ka", "kbp", "kea", "kk", "ky", "mn", "ms", "plt", "ps", "tt", "tw", "uk", "ug",  "vi", "xh"]
 NLTK_PUNKT_LANGS = {"no": "norwegian",
                     "et": "estonian",
                     "da": "danish",
