45ec37d74609a2b3faa64d5949284f2748e33221
mbanon <mbanon@prompsit.com>
2024-08-28 13:45:12 +0000

    Support for Tumbuka

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -34,7 +34,7 @@ MOSES_LANGS = ["ba", "br", "ca", "cs", "co", "de", "el", "en", "es", "fi", "fr",
 
 NLTK_WORD_LANGS = ["ar", "as", "ayr", "az", "ban", "be", "bm", "bem", "ceb", "dik","dyu", "dz", "fa", "fuv", "gaz", "ht", "hy", "jv",
                     "ka", "kab", "kac", "kam", "kbp", "kea", "kg", "kk", "kmb", "ky", "li", "lij", "lmo",  "ltg", "lus", "min", "mn", "mni",
-                    "ms", "nso", "ny", "oc", "pag", "plt", "ps","rn", "rw", "sc", "scn", "sg", "sm", "tk","tn", "tpi", "tt", "tw", "tzm",
+                    "ms", "nso", "ny", "oc", "pag", "plt", "ps","rn", "rw", "sc", "scn", "sg", "sm", "tk","tn", "tpi", "tt", "tw", "tzm", "tum",
                     "uk", "ug", "umb", "vec", "vi", "xh", "yo", "zu"]
 NLTK_PUNKT_LANGS = {"no": "norwegian",
                     "et": "estonian",
