6d62baff7e2f599695107c771362e7cf74aa7acf
mbanon <mbanon@prompsit.com>
2024-08-29 15:26:17 +0000

    Support for Fon

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -32,7 +32,7 @@ logging.disable(logging.NOTSET)
 
 MOSES_LANGS = ["as", "ba", "br", "ca", "cs", "co", "de", "el", "en", "es", "fi", "fr", "ga", "hu", "is", "it", "lt", "lv", "mni", "nl", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "ta"]
 
-NLTK_WORD_LANGS = ["ace", "ar", "ayr", "az", "ban", "be", "bjn", "bm", "bem", "ceb", "crh", "dik","dyu", "dz", "fa", "fo", "fuv", "gaz", "gd", "ha", "ht", "hy","ilo", "jv",
+NLTK_WORD_LANGS = ["ace", "ar", "ayr", "az", "ban", "be", "bjn", "bm", "bem", "ceb", "crh", "dik","dyu", "dz", "fa", "fo", "fon", "fuv", "gaz", "gd", "ha", "ht", "hy","ilo", "jv",
                     "ka", "kab", "kac", "kam", "kbp", "kea", "kg", "ki", "kk", "kmb", "knc", "ky", "li", "lij", "lmo",  "ltg", "luo",  "lus", "mi", "min", "mn", 
                     "ms", "nso", "ny", "oc", "pag", "pap", "plt", "ps","quy", "rn", "rw", "sc", "scn", "sd", "sg", "sm", "ss", "su",
                     "tg", "ti", "tk", "tn", "tpi", "ts", "tt", "tw", "tzm", "tum", "uk", "ug", "umb", "vec", "vi", "war", "wo", "xh", "yo", "zu"]
