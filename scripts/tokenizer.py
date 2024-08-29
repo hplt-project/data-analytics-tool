c687efb80419fd8bc51b4628652d0e5fa5ca77c6
mbanon <mbanon@prompsit.com>
2024-08-29 09:07:24 +0000

    Support for Awadhi

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -83,7 +83,7 @@ BNLP_LANGS = ["bn"]
 
 THAI_LANGS = ["th"]
 
-INDIC_LANGS = ["gu" ,"hi", "kn", "mag", "mai", "mr", "ne", "pa", "te", "ur"]
+INDIC_LANGS = ["awa", "gu" ,"hi", "kn", "mag", "mai", "mr", "ne", "pa", "te", "ur"]
 
 PKUSEG_LANGS = ["zh", "zh-Hant"]
 
