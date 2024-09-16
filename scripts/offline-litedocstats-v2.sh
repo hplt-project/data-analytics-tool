69557b46312f5a26f5798318aff1bca30c0d6b0a
Marta Bañón <mbanon@prompsit.com>
2024-09-16 17:12:03 +0000

    Added missing script for lite docstats v2

---
+++ scripts/offline-litedocstats-v2.sh
@@ -0,0 +1,8 @@
+L2=$1
+L3=$2
+zstdcat /work/uploaded_corpora/$L3*.jsonl.zst | python3.10 /work/scripts/readdocuments.py - /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/yaml_dir/HPLT-v2-"$L3".lite.yaml  \
+$L2 --langs /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 --fluency /work/uploaded_corpora/HPLT-v2-"$L3".tsv.classify  && \
+cat /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 | sort | uniq -c | sort -nr > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.langcounts &&\
+python3.10 /work/scripts/readcorpus_mono.py /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/yaml_dir/HPLT-v2-"$L3".lite.yaml $L2 --lite --debug
+
+
