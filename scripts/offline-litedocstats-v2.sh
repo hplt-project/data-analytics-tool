7aa4d0d0c0c9127b80a7349a5e4a2cfd5e257a1d
mbanon <mbanon@prompsit.com>
2024-09-17 21:12:35 +0000

    Fix issue on generating hardrules for lite stats

--- scripts/offline-litedocstats-v2.sh
+++ scripts/offline-litedocstats-v2.sh
@@ -12,6 +12,6 @@ echo "Generating langcounts..."
 cat /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 | sort | uniq -c | sort -nr > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.langcounts
 source /work/venvs/venv-mc/bin/activate
 echo "Running Monocleaner  Hardrules..."
-cat /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 | /work/preprocess/build/bin/cache -k 1  parallel -k -j $JOBS --pipe monocleaner-hardrules --score_only --annotated_output --run_all_rules --disable_lang_ident  $L2 - - > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.hardrules 2> hr.log
+cat /work/uploaded_corpora/HPLT-v2-$L3.tsv | /work/preprocess/build/bin/cache -k 1  parallel -k -j $JOBS --pipe monocleaner-hardrules --score_only --annotated_output --run_all_rules --disable_lang_ident  $L2 - - > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.hardrules 2> hr.log
 deactivate
 python3.10 /work/scripts/readcorpus_mono.py /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/yaml_dir/HPLT-v2-"$L3".lite.yaml $L2 --lite --debug
