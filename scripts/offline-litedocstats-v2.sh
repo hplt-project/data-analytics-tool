9443df4010d50705f50d4637e801a5b73b9a1199
mbanon <mbanon@prompsit.com>
2024-09-18 10:54:25 +0000

    Added langid to lite stats

--- scripts/offline-litedocstats-v2.sh
+++ scripts/offline-litedocstats-v2.sh
@@ -7,9 +7,14 @@ JOBS=$(($JOBS>1 ? $JOBS : 1))
 
 
 echo "Reading documents..."
-zstdcat /work/uploaded_corpora/$L3*.jsonl.zst | python3.10 /work/scripts/readdocuments.py - /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/yaml_dir/HPLT-v2-"$L3".lite.yaml $L2 --langs /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 
+zstdcat /work/uploaded_corpora/$L3*.jsonl.zst | python3.10 /work/scripts/readdocuments.py - /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/yaml_dir/HPLT-v2-"$L3".lite.yaml $L2 #--langs /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 
+echo "Running FastSpell..."
+python3.10 /work/scripts/force-fasttext-download.py $L2
+./scripts/parallel-fastspell.sh $JOBS $L2 /work/uploaded_corpora/HPLT-v2-"$L3".tsv /work/uploaded_corpora/HPLT-v2-"$L3".tsv.langids 1 
 echo "Generating langcounts..."
-cat /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2 | sort | uniq -c | sort -nr > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.langcounts
+cat /work/uploaded_corpora/HPLT-v2-"$L3".tsv.langids | sort --parallel $JOBS | uniq -c | sort -nr  >  /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.langcounts
+#        #nyapa
+#        cp $saved_file_path.$srclang.langcounts $tsv_file_path.$srclang.langcounts      
 source /work/venvs/venv-mc/bin/activate
 echo "Running Monocleaner  Hardrules..."
 cat /work/uploaded_corpora/HPLT-v2-$L3.tsv | /work/preprocess/build/bin/cache -k 1  parallel -k -j $JOBS --pipe monocleaner-hardrules --score_only --annotated_output --run_all_rules --disable_lang_ident  $L2 - - > /work/uploaded_corpora/HPLT-v2-$L3.tsv.$L2.hardrules 2> hr.log
