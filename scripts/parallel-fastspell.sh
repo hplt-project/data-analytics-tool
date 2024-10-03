6fa799c43b7943df223495a45a072662d3adf604
mbanon <mbanon@prompsit.com>
2024-10-03 07:30:28 +0000

    Optional --nocache flag to avoid caching in memory for fastspell and hardrules

--- scripts/parallel-fastspell.sh
+++ scripts/parallel-fastspell.sh
@@ -5,6 +5,11 @@ inputfile=$3
 outputfile=$4
 column=$5
 
-cat $inputfile | cut -f $column | /work/preprocess/build/bin/cache -k 1 parallel -k -j $JOBS --pipe ./scripts/par-fastspell.sh $langcode > $outputfile
+if [[ $* == *--nocache* ]]
+then
+	cat $inputfile | cut -f $column |parallel -k -j $JOBS --pipe ./scripts/par-fastspell.sh $langcode > $outputfile
+else
+	cat $inputfile | cut -f $column | /work/preprocess/build/bin/cache -k 1 parallel -k -j $JOBS --pipe ./scripts/par-fastspell.sh $langcode > $outputfile
+fi
 
 
