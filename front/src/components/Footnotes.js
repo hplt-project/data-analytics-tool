ea2296f96cf2267bb5a1f6522bfc02ec9b3c7682
Lukas <lweymann@prompsit.com>
2025-03-18 09:51:39 +0100

    Added register labels equivalence tables to PDF footnote

--- front/src/components/Footnotes.js
+++ front/src/components/Footnotes.js
@@ -72,7 +72,8 @@ export default function Footnotes() {
         <h4>Distribution of documents by document score</h4>
         <ul>
           <li>
-          Obtained with Web Docs Scorer (https://github.com/pablop16n/web-docs-scorer/).
+            Obtained with Web Docs Scorer
+            (https://github.com/pablop16n/web-docs-scorer/).
           </li>
         </ul>
       </div>
@@ -88,7 +89,10 @@ export default function Footnotes() {
       <div className={styles.singleNote}>
         <h4>Segment noise distribution</h4>
         <ul>
-          <li>Obtained with Bicleaner Hardrules (https://github.com/bitextor/bicleaner-hardrules/).</li>
+          <li>
+            Obtained with Bicleaner Hardrules
+            (https://github.com/bitextor/bicleaner-hardrules/).
+          </li>
         </ul>
       </div>
       <div className={styles.singleNote}>
@@ -102,6 +106,144 @@ export default function Footnotes() {
             https://github.com/hplt-project/data-analytics-tool/blob/main/scripts/resources/README.txt
           </li>
         </ul>
+      </div>
+      <div className={styles.singleNote}>
+        <h4>Register labels</h4>
+        <div
+          className={styles.registarLebelsTables}
+          style={{ display: "flex" }}
+        >
+          <table style={{ marginRight: "20px" }}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Abbr.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className={styles.mainCategory}>Machine-translated</td>
+                <td>MT</td>
+              </tr>
+              <tr>
+                <td className={styles.mainCategory}>Lyrical</td>
+                <td>LY</td>
+              </tr>
+              <tr>
+                <td className={styles.mainCategory}>Spoken</td>
+                <td>SP</td>
+              </tr>
+              <tr>
+                <td>Interview</td>
+                <td>it</td>
+              </tr>
+              <tr>
+                <td className={styles.mainCategory}>Interactive discussion</td>
+                <td>ID</td>
+              </tr>
+              <tr>
+                <td className={styles.mainCategory}>Narrative</td>
+                <td>NA</td>
+              </tr>
+              <tr>
+                <td>News report</td>
+                <td>ne</td>
+              </tr>
+              <tr>
+                <td>Sports report</td>
+                <td>sr</td>
+              </tr>
+              <tr>
+                <td>Narrative blog</td>
+                <td>nb</td>
+              </tr>
+            </tbody>
+          </table>
+          <table style={{ marginRight: "20px" }}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Abbr.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className={styles.mainCategory}>
+                <td>How-to or instructions</td>
+                <td>HI</td>
+              </tr>
+              <tr>
+                <td>Recipe</td>
+                <td>re</td>
+              </tr>
+              <tr className={styles.mainCategory}>
+                <td>Informational persuasion</td>
+                <td>IP</td>
+              </tr>
+              <tr>
+                <td>Description with intent to sell</td>
+                <td>ds</td>
+              </tr>
+              <tr>
+                <td>News & opinion blog or editorial</td>
+                <td>ed</td>
+              </tr>
+              <tr className={styles.mainCategory}>
+                <td>Informational description</td>
+                <td>IN</td>
+              </tr>
+              <tr>
+                <td>Enciclopedia article</td>
+                <td>en</td>
+              </tr>
+              <tr>
+                <td>Research article</td>
+                <td>ra</td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Abbr.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Description of a thing or person</td>
+                <td>dtp</td>
+              </tr>
+              <tr>
+                <td>FAQ</td>
+                <td>fi</td>
+              </tr>
+              <tr>
+                <td>Legal terms & conditions</td>
+                <td>lt</td>
+              </tr>
+              <tr className={styles.mainCategory}>
+                <td>Opinion</td>
+                <td>OP</td>
+              </tr>
+              <tr>
+                <td>Review</td>
+                <td>rv</td>
+              </tr>
+              <tr>
+                <td>Opinion blog</td>
+                <td>ob</td>
+              </tr>
+              <tr>
+                <td>Denominational religious blog or sermon</td>
+                <td>rs</td>
+              </tr>
+              <tr>
+                <td>Advice</td>
+                <td>av</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
