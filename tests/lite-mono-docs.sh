ba43fc76a7360fea4d79ca09377d0b5f1d3d7061
mbanon <mbanon@prompsit.com>
2025-08-25 09:31:38 +0000

    Support for HPLTv3 documents

--- tests/lite-mono-docs.sh
+++ tests/lite-mono-docs.sh
@@ -2,5 +2,5 @@
 cd /work/uploaded_corpora/
 wget https://data.hplt-project.org/two/cleaned/ast_Latn/1.jsonl.zst -O ast_Latn.jsonl.zst
 cd ..
-bash /work/scripts/runstats.sh uploaded_corpora/ast_Latn.jsonl.zst  yaml_dir/test.lite-mono-docs.yaml ast - hplt mono --skip-register-labels
+bash /work/scripts/runstats.sh uploaded_corpora/ast_Latn.jsonl.zst  yaml_dir/test.lite-mono-docs.yaml ast - hplt2 mono --skip-register-labels --skip-domain-labels
 rm uploaded_corpora/ast_Latn.*
